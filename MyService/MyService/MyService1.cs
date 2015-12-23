6362e10959472ce6f4a655b66db3273be03d0a79
amete <anirbanmete@gmail.com>
2015-12-23 17:50:54 +0530

    This is a WCF Test Project
    WCF Project

---
+++ MyService/MyService/MyService1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace MyService
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        string empID;
+
+        [DataMember]
+        string empName;
+
+        [DataMember]
+        public string EmpID { get; set; }
+
+        [DataMember]
+        public string EmpName { get; set; }
+    }
+    
+    [ServiceContract]
+    public interface IEmployee
+    {
+        [OperationContract]
+        string HiEmployee(Employee employee);
+        
+    }
+    class MyService1
+    {
+        public string HiEmployee(Employee employee)
+        {
+            return string.Format( "HiEmployee {0} {1}",employee.EmpID,employee.EmpName );
+
+        }
+    }
+}
