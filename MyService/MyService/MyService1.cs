822032fcfde8b3767201381c0acf575b6146ba75
amete <anirbanmete@gmail.com>
2015-12-28 11:15:15 +0530

    Test

--- MyService/MyService/MyService1.cs
+++ MyService/MyService/MyService1.cs
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
+﻿
 ﻿/**************************************WCF Project*************************************/
 
 
 
-=======
->>>>>>> 11d25840b5c81212e91fe32c0640408d5c9ffe89
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,13 +39,13 @@ namespace MyService
     {
         public string HiEmployee(Employee employee)
         {
-<<<<<<< HEAD
+
             return string.Format( "Hi Employee {0} {1}",employee.EmpID,employee.EmpName );
 
-=======
+
             return string.Format( "Hi Employe.You have done great job{0} {1}",employee.EmpID,employee.EmpName );
-            //********Changes  Made
->>>>>>> 11d25840b5c81212e91fe32c0640408d5c9ffe89
+            //********Changes  Made by Soumava
+
         }
     }
 }
